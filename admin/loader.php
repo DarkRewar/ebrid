8fdd2d78c4da4362d49f73d166c810f42197f7a4
Curtis Pelissier <curtis.pelissier@lignusdev.com>
2015-01-21 17:10:55 +0100

    Ajout des functions et de l'admin

--- admin/loader.php
+++ admin/loader.php
@@ -6,11 +6,17 @@
  *  @package Ebrid
  */
 
-require_once( dirname(dirname(__FILE__)) . '/include/loader.function.php');
+/**
+ *  We need to define a messages warning, success or error
+ *  to treat those later or to prevent the user.
+ */
+$_messages = array();
+
+require_once( dirname(dirname(__FILE__)) . '/include/loader.function.php' );
 
 if(is_request(__FILE__)) redirect('admin-panel');
 
-require_once( dirname(dirname(__FILE__)) . '/include/loader.class.php');
-require_once( dirname(__FILE__) . '/include/loader.class.php');
+require_once( dirname(dirname(__FILE__)) . '/include/loader.class.php' );
+require_once( dirname(__FILE__) . '/include/loader.class.php' );
 
-require_once( dirname(dirname(__FILE__)) . '/include/db.php');
+require_once( dirname(dirname(__FILE__)) . '/include/db.php' );
