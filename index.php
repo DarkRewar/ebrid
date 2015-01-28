564f59250b8c535173f13bd4a162382bcee6fc33
Enzo Cornand <goulaheau@gmail.com>
2015-01-28 16:42:19 +0100

    no message

--- index.php
+++ index.php
@@ -6,23 +6,4 @@
  *  @package Ebrid
  */
 
-<<<<<<< HEAD
 require_once(dirname(__FILE__) . '/loader.php');
-?>
-<!doctype html>
-<html lang="fr">
-<head>
-  <meta charset="utf-8">
-  <title>Titre de la page</title>
-  <link rel="stylesheet" href="style.css">
-  <script src="script.js"></script>
-</head>
-<body>
-  ...
-  <!-- Le reste du contenu -->
-  ...
-</body>
-</html>
-=======
-require( dirname(__FILE__) . '/loader.php');
->>>>>>> cca2ab93f03204a4f537b21575d3482f863e68d1
