4b51c3c8d7c2c60a652629ae29775e07e8c9e0c4
Curtis Pelissier <curtis.pelissier@laposte.net>
2015-02-27 18:47:11 +0100

    JS correction bug

--- admin/js/new-post.js
+++ admin/js/new-post.js
@@ -3,13 +3,13 @@ loadScripts(scripts2load);
 
 function checkParent(id){
     $('input[value="'+id+'"]').prop('checked', true);
-    if($('input[value="'+id+'"]').data('parent').length > 0)
+    if($('input[value="'+id+'"]').data('parent') > 0)
         checkParent($('input[value="'+id+'"]').data('parent'));
 }
 
 function checkSons(id){
     $('input[data-parent="'+id+'"]').prop('checked', false);
-    if($('input[data-parent="'+id+'"]').data('parent').length > 0)
+    if($('input[data-parent="'+id+'"]').data('parent') > 0)
         checkSons($('input[data-parent="'+id+'"]').val());
 }
 
