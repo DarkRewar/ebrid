e4a771059d183eb00b84ce39e91d67cd1b42b920
Emmanuel Marti <emmanuel.marti@y-nov.com>
2015-03-18 17:39:59 +0100

    pb d'affichage action dans le href

--- admin/affichage-article.php
+++ admin/affichage-article.php
@@ -6,31 +6,30 @@
  *  @package Ebrid
  */
 
-require_once( dirname(__FILE__) . '/loader.php');
+require_once (dirname(__FILE__) . '/loader.php');
 
 get_header_admin("Affichage des articles");
 
-if(isset($_GET['action'])){
+if (isset($_GET['action'])) 
+{
     BlogArticle::_deleteArticle($_GET['article']);
 }
-
-
-
 ?>
 <body>
     <div>
         <?php
-            foreach(BlogArticle::_getArticles() as $v){
-                echo "<a href=\"new-post.php?article=".$v['ida']."\">".$v['title']."</a>
-                <button class=\"activeArticle\" id='active_article_".$v['ida']."'>Activer</button>
-                <button class=\"supprimerArticle\" id='supprimer_article_".$v['ida']."'>
-                    <a href=\"affichage-article.php?article=".$v['ida']."&&action=\"trashed\">Supprimer
+foreach (BlogArticle::_getArticles() as $v) 
+{
+    echo "<a href=\"new-post.php?article=" . $v['ida'] . "\">" . $v['title'] . "</a>
+                <button class=\"activeArticle\" id='active_article_" . $v['ida'] . "'>Activer</button>
+                <button class=\"supprimerArticle\" id='supprimer_article_" . $v['ida'] . "'>
+                    <a href=\"affichage-article.php?article=" . $v['ida'] . "&&action=\"trashed\">Supprimer
                     </a>
                 </button>
                 <br />
-                 ";  
-            }
-        ?>
+                 ";
+}
+?>
 
     </div>
     <script type="text/javascript" src="./js/mod/active_article.js"></script>
